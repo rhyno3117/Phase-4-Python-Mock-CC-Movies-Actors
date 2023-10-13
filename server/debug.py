075947123ccc31772c0e676961b87d58ebacea69
Sam Waters <samuel.waters@gmail.com>
2023-10-12 19:26:45 -0600

    seed.py

--- server/debug.py
+++ server/debug.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
 from faker import Faker
 
-# from app import app
-# from models import db, Movie, Actor, Credit
-
-from faker.providers import lorem
-
-fake = Faker()
-
-fake.add_provider(lorem)
-
+from app import app
+from models import db, Movie, Actor, Credit
 
 
 
 if __name__ == '__main__':
-    print(fake.paragraph(nb_sentences=2))
+    pass
